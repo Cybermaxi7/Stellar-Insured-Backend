e6d5f14bc38c9317892901e49ea48157d1d631fc
gabito1451 <obiagaziegabriel@gmail.com>
2026-01-22 05:52:41 -0800

    Implementthe notification infrastructure and data model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './common/database/database.module';
 import { HealthModule } from './modules/health/health.module';
+import { NotificationModule } from './modules/notification/notification.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule, HealthModule],
+  imports: [ConfigModule, DatabaseModule, HealthModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
